f1cde3d32953de8535fa85a50a83350ea2e9ea22
Zavyalova Varvara <zvs1110@gmail.com>
2024-11-20 21:13:15 +0300

    changes functions

--- test_shell_emulator.py
+++ test_shell_emulator.py
@@ -6,33 +6,59 @@ class TestShellEmulator(unittest.TestCase):
     def setUp(self):
         self.emulator = ShellEmulator("config.json")
 
-    def test_ls(self):
+    def test_ls_files(self):
         result = self.emulator.ls([])
         self.assertIn("/home/testuser/file1.txt", result)
         self.assertIn("/home/testuser/file2.txt", result)
-        self.assertNotIn("No files found.", result)
 
-    def test_cd(self):
-        result = self.emulator.cd(["home/testuser"])
+    def test_ls_no_files(self):
+        self.emulator.current_dir = "/empty_dir"
+        result = self.emulator.ls([])
+        self.assertEqual(result, "No files found.")
+
+    def test_cd_change_to_absolute(self):
+        result = self.emulator.cd(["/home/testuser"])
         self.assertEqual(self.emulator.current_dir, "/home/testuser")
+        self.assertIn("Changed to directory", result)
 
-        result = self.emulator.cd([".."])
-        self.assertEqual(self.emulator.current_dir, "/home")
+    def test_cd_directory_not_found(self):
+        result = self.emulator.cd(["/non_existent_dir"])
+        self.assertEqual(result, "Directory '/non_existent_dir' not found.")
 
-        result = self.emulator.cd(["folder"])
-        self.assertEqual(result, "Directory 'folder' not found.")
+    def test_exit(self):
+        result = self.emulator.exit_shell([])
+        self.assertEqual(result, "Exiting shell.")
+
+    def test_exit_logs_action(self):
+        self.emulator.exit_shell([])
+        with open(self.emulator.log_file, 'r') as f:
+            lines = f.readlines()
+            self.assertIn("exit", lines[-1])
 
     def test_uniq(self):
-        result = self.emulator.uniq(["apple\napple\nbanana\nbanana"])
-        self.assertEqual(result, "apple\nbanana")
+        input_data = "apple\nbanana\napple\norange"
+        result = self.emulator.uniq([input_data])
+        self.assertEqual(result, "apple\nbanana\norange")
+
+    def test_uniq_no_input(self):
+        result = self.emulator.uniq([])
+        self.assertEqual(result, "No input provided.")
 
     def test_whoami(self):
         result = self.emulator.whoami([])
-        self.assertEqual(result, "username")
+        self.assertEqual(result, self.emulator.user)
 
-    def test_echo(self):
+    def test_whoami_config_user(self):
+        self.assertEqual(self.emulator.user, "testuser")  # Предполагаем, что в конфиге задано имя 'testuser'
+
+    def test_echo_single(self):
+        result = self.emulator.echo(["Hello"])
+        self.assertEqual(result, "Hello")
+
+    def test_echo_multiple(self):
         result = self.emulator.echo(["Hello", "world!"])
         self.assertEqual(result, "Hello world!")
+
 
 if __name__ == '__main__':
     unittest.main()
